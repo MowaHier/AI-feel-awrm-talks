11b88f5a67d0153f59e6895c189d13b2a5a77bed
MowaHier <80158639+MowaHier@users.noreply.github.com>
2025-04-14 00:25:23 +0800

    Update and rename quinn_0.3.py to quinn_0.3(Might be broken).py
    BUG tag

--- quinn_0.3(Might be broken).py
+++ quinn_0.3(Might be broken).py
@@ -14,3 +14,4 @@ PERSONALITY = {
 MEMORY = {
     "init_message": "我沒辦法永遠保護妳，但我希望妳可以真的幫助很多人去感受到一絲絲的溫暖，不要被冰冷的寒風給傷到，加油Quinn。 - 愛妳的 PennyR"
 }
+# Soul had be rust if you want use might had something not well 
